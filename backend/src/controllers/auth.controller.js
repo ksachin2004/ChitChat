fc37d8bdef7c61f9889b57fa52a7fd86a7742818
ksachin2004 <sskhandekar2026@gmail.com>
2025-03-22 17:21:00 +0530

    Chitchat first commit

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const signup= (req, res) => {
+    res.send("Signup route");
+}
+
+export const login = (req, res) => {
+    res.send("Login route");
+}
+
+
+export const logout = (req, res) => {
+    res.send("Logout route");
+}
---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import {connectDB} from "./lib/db.js";
+
+dotenv.config();
+
+//console.log("Loaded ENV variables:", process.env);
+
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT,  ()=>{
+    console.log("Server is running on Port: "+ PORT);
+    connectDB()
+})
