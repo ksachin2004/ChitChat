44486ecf9966f87f485ce01a6c957b4e5dd864b2
ksachin2004 <sskhandekar2026@gmail.com>
2025-03-24 20:49:01 +0530

    Supposedly built complete backend (maybe...Let's see)

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import {connectDB} from "./lib/db.js";
 import cookieParser from "cookie-parser";
-import cors from "cors";
-
+//import cors from "cors";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 //console.log("Loaded ENV variables:", process.env);
-
-import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
@@ -15,15 +14,16 @@ const app = express();
 const PORT = process.env.PORT;
 
 // CORS settings to allow credentials (cookies)
-app.use(cors({
-    origin: "http://localhost:3000", // Change this to your frontend URL
-    credentials: true
-}));
+// app.use(cors({
+//     origin: "http://localhost:3000", // Change this to your frontend URL
+//     credentials: true
+// }));
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT,  ()=>{
     console.log("Server is running on Port: "+ PORT);
